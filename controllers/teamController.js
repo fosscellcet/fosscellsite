9e695a2ece56eb8b11f6b85d67f086a375c23c56
Gopal <gopalkurup@gmail.com>
2018-08-25 11:35:15 +0530

    init team model

---
+++ controllers/teamController.js
@@ -0,0 +1,18 @@
+var model = require('../models').team;
+var Promise = require('bluebird');
+
+
+teamController = {};
+
+teamController.getAllTeamMembers = () => new Promise((resolve,reject) => {
+        model.findAll()
+            .then((teamMembers) => {
+                resolve(teamMembers);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+    });
+
+
+module.exports = teamController;
