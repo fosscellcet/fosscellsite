7125d92ce2a711fd3cb6a85fd9363be31f264bce
Bilal <bilaljaleel123@gmail.com>
2018-08-26 18:43:33 +0530

    Added api to routes

--- routes/api/blog/index.js
+++ routes/api/blog/index.js
@@ -1,14 +1,12 @@
 var express = require('express')
 var router = express.Router()
-var controller = require('../../controllers/blogController')
+var controller = require('../../../controllers/blogController')
 
 router.get('/', (req, res, next) => {
     controller.getLatestPosts(1)
         .then((posts) => {
             console.log("THe posts are" + posts)
-            res.json(
-                { posts }
-            )
+            res.json({ posts })
         }).catch((err) => {
             console.log(err)
             res.json({ 'message': 'An error has occurred', 'error': err })
