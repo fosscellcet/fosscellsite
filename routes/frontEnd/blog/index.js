6016a3f46e69ef8386c221e30844adcfbb04ef9e
Bilal <bilaljaleel123@gmail.com>
2018-08-27 17:50:38 +0530

    Added vuejs

--- routes/frontEnd/blog/index.js
+++ routes/frontEnd/blog/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router()
-var filePath = '/views/test.html'
+var filePath = '/views/pages/blog/index.html'
 var resolvedPath = '/home/bilal/program/FOSS/fosscellsite' + filePath
 router.get('/', (req, res) => {
     res.sendFile(resolvedPath)
